ee58deb2f4fd0a0defe02c5418d5bcfb4a05ef65
Clint <clintkunz@gmail.com>
2019-05-10 13:37:37 -0600

    create board and copy board functions

--- connectlogic.py
+++ connectlogic.py
@@ -1,6 +1,6 @@
 # initial game logic source - https://www3.nd.edu/~pbui/teaching/cdt.30010.fa16/project01.html
 
-from IPython.display import display, HTML, clear_output
+# from IPython.display import display, HTML, clear_output
 import random
 import time
 
@@ -24,3 +24,27 @@ directions = (
     (1, -1), (1, 0), (1, 1)
 )
 
+# functions
+
+def create_board(rows=rows, columns=columns):
+    board = []
+
+    for row in range(rows):
+        board_row = []
+        for column in range(columns):
+            board_row.append(piece_none)
+            board.append(board_row)
+
+    return board
+
+    def copy_board(board):
+        rows = len(board)
+        columns = len(board[0])
+        copied = create_board(rows, columns)
+
+        for row in range(rows):
+            for column in range(columns):
+                copied[row][column] = board[row][column]
+        return copied
+
+print(create_board())
