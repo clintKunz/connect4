9efe708eacf33214d4cdf9c473802053a75bb84d
Clint <clintkunz@gmail.com>
2019-05-13 12:42:47 -0600

    added ui structure and basic logic

--- connect4/src/App.js
+++ connect4/src/App.js
@@ -1,26 +1,96 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Container = styled.div`
+  border: 1px solid red; 
+  min-width: 400px; 
+  max-width: 1100px;
+  width: 100%; 
+`;
+
+const Header = styled.h1`
+  display: flex;
+  justify-content: center; 
+  color: blue;
+`;
+
+const Player = styled.h3`
+  display: flex; 
+  justify-content: space-between; 
+  width: 60%;
+  margin: 0 auto; 
+  border: 1px solid blue; 
+`;
+
+const Board = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center; 
+  width: 60%; 
+  margin: 30px auto; 
+`;
+
+const Row = styled.div`
+  background-color: yellow;
+  width: 100%; 
+`;
+
+const Cell = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center; 
+`;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      player1: 1,
+      player2: 2,
+      turn: true,
+      board: []
+    }
+  }
+
+  componentDidMount() {
+    let board = [];
+    for (let r = 0; r < 6; r++) {
+      let row = [];
+      for (let c = 0; c < 7; c++) { row.push(null) }
+      board.push(row);
+    }
+
+    this.setState({
+      board
+    })
+  }
+
+  render() {
+    return (
+      <Container>
+        <Header>Connect 4!</Header> 
+        <Player>
+          <div style={{color: 'red'}}>
+            Player 1
+            {this.state.turn? <div>Your Turn</div>: null}
+          </div>
+          <div style={{color: 'green'}}>
+            Player 2
+            {!this.state.turn? <div>Your Turn</div>: null}
+          </div>
+        </Player>
+        <Board>
+          {this.state.board.map(row => (
+            <Row>
+            {row.map(cell => (
+              <Cell>{cell === null? 'e' : cell === 1? 'x': 'o'}</Cell>
+            ))}
+            </Row>
+          ))}
+        </Board>
+      </Container>
+    );
+  }
 }
 
 export default App;
