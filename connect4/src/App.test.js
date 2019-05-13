8ba0b30b58be22bed758fb536300cba28c7bace9
Clint <clintkunz@gmail.com>
2019-05-13 15:41:02 -0600

    added a jest test

--- connect4/src/App.test.js
+++ connect4/src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render } from 'react-testing-library';
+import 'jest-dom/extend-expect'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders turn text', () => {
+  const { getByText } = render(<App/>)
+  const turn = getByText(/turn/i)
+  expect(turn).toBeInTheDocument()
+})
